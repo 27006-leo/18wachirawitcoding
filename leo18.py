d0450d7a054ee6b5be0c9f7a06e63926dcb4934b
27006-leo <27006@bbw.ac.th>
2026-06-25 14:36:51 +0700

    Add files via upload
    25/6/69

--- leo18.py
+++ leo18.py
@@ -1,6 +1,20 @@
-a = int(20)
-b = float(3.5)
-c = a + b
-d = a - int(b)
-e = b * 2
-print(c, d, e)
+print("คํานวณbmi")
+
+point1 = int(input("นํ้าหนัก"))
+point2 = int(input("ส่วนสูง"))
+
+point1 / ( point2 * point1 )
+print("ค่านํ้าหนัก",point1,point2)
+average = point1 / (point2 * point1)
+print("ค่าเฉลี่ยbmi")
+if average>=25:
+    print("อ้วน")
+
+elif average>=24.9:
+    print("นํ้าหนักเกิน")
+
+elif average>=22.9:
+    print("ปกติ")
+
+else:
+    print("นํ้าหนักน้อย")
